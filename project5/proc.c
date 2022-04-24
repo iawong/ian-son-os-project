3c1c27d8a40714a1742daacabdc2d878c6ac614e
Ian <ian@Ians-iMac.local>
2022-04-24 19:10:16 -0400

    removed the copies of structs, only need the first line for that stuff to carry over

--- project5/proc.c
+++ project5/proc.c
@@ -1,18 +1,4 @@
-/*
- * The Process Control Block
- */
-struct PCB {
-	char name[7];  		// 6 chars for filename + 1 for null terminator.
-	int state;		// One of the constants above.
-	int segment;		// memory segment occupied by the process.
-	int stackPointer;	// address of the stack pointer for the process.
-	
-	/*
-	 * Pointers to allow PCB's to be stored in doubly linked lists.
- 	 */
-	struct PCB *next;
-	struct PCB *prev;
-};
+#include "proc.h"
 
 /*
  * This function initializes all of the global variables and 
@@ -24,42 +10,56 @@ struct PCB {
  * set to 0x0000.  running should refer to the PCB for the idle process and the ready queue
  * should be empty.
  */  
-void initializeProcStructures();
+void initializeProcStructures() {
+
+}
 
 /*
  * Returns the index of the first free memory segment or -1 if 
  * all of the memory segments are used.  The returned memory segment
  * should be marked as used.
  */
-int getFreeMemorySegment();
+int getFreeMemorySegment() {
+
+}
 
 /*
  * Release the memory segment indicated by the given index into
  * the memory map.
  */
-void releaseMemorySegment(int seg);
+void releaseMemorySegment(int seg) {
+
+}
 
 /*
  * Return a pointer to an available PCB from the PCB pool.  All of the
  * available PCBs should be marked as DEFUNCT.  The returned PCB should
  * be marked as STARTING. Return NULL if there are no PCBs available.
  */
-struct PCB *getFreePCB();
+struct PCB *getFreePCB() {
+
+}
 
 /*
  * Release the provided PCB by setting its state to DEFUNCT, setting
  * its next and prev pointers to NULL and setting the first character
  * of its name to 0x00.
  */
-void releasePCB(struct PCB *pcb);
+void releasePCB(struct PCB *pcb) {
+
+}
 
 /*
  * Add the provided PCB to the tail of the ready queue.
  */
-void addToReady(struct PCB *pcb);
+void addToReady(struct PCB *pcb) {
+
+}
 
 /*
  * Remove the PCB at the head of the ready queue and return a 
  * pointer to it.
  */
-struct PCB *removeFromReady();
+struct PCB *removeFromReady() {
+    
+}
