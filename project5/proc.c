1161a7ab6586928dc9341504571b8fdd1bfc8daf
Ian <ian@Ians-iMac.local>
2022-04-24 12:58:43 -0400

    copied all prototype functions into proc.c

--- project5/proc.c
+++ project5/proc.c
@@ -14,3 +14,52 @@ struct PCB {
 	struct PCB *prev;
 };
 
+/*
+ * This function initializes all of the global variables and 
+ * structures defined above.  All entries in the memory map should be marked as FREE.
+ * All of the PCBs in the pcbPool should have the first character of their 
+ * names set to null, their state set to DEFUNCT and their segment and stack
+ * pointers set to 0x0000. The PCB for the idle process should be initialized 
+ * with the name IDLE, the state READY, and both the segment and stack pointers
+ * set to 0x0000.  running should refer to the PCB for the idle process and the ready queue
+ * should be empty.
+ */  
+void initializeProcStructures();
+
+/*
+ * Returns the index of the first free memory segment or -1 if 
+ * all of the memory segments are used.  The returned memory segment
+ * should be marked as used.
+ */
+int getFreeMemorySegment();
+
+/*
+ * Release the memory segment indicated by the given index into
+ * the memory map.
+ */
+void releaseMemorySegment(int seg);
+
+/*
+ * Return a pointer to an available PCB from the PCB pool.  All of the
+ * available PCBs should be marked as DEFUNCT.  The returned PCB should
+ * be marked as STARTING. Return NULL if there are no PCBs available.
+ */
+struct PCB *getFreePCB();
+
+/*
+ * Release the provided PCB by setting its state to DEFUNCT, setting
+ * its next and prev pointers to NULL and setting the first character
+ * of its name to 0x00.
+ */
+void releasePCB(struct PCB *pcb);
+
+/*
+ * Add the provided PCB to the tail of the ready queue.
+ */
+void addToReady(struct PCB *pcb);
+
+/*
+ * Remove the PCB at the head of the ready queue and return a 
+ * pointer to it.
+ */
+struct PCB *removeFromReady();
