cf0c3a5b522eabcc364fda94a28ed6903c15590e
iawong <ianwong1020@gmail.com>
2022-04-29 16:03:24 -0400

    modified terminate and adjusted handleTimerInterrupt

--- project5/kernel.c
+++ project5/kernel.c
@@ -359,10 +359,13 @@ int executeProgram(char* name) {
 
 // terminate a running program by resetting the segments
 void terminate() {
-    resetSegments();
-    interrupt(0x21, 0x04, "shell\0", 0x2000, 0);
-    // printString("I'm back!\0");
-    // while(1);
+    int segment;
+    // resetSegments();
+    segment = running->segment;
+    running->state = DEFUNCT;
+    memoryMap[(segment - 0x2000) / 0x1000] = FREE;
+
+    while(1);
 }
 
 // write the contents of buffer into a sector
@@ -513,16 +516,24 @@ void handleTimerInterrupt(int segment, int sp) {
     struct PCB *pcbSaved; // this is the new process we're adding to the ready queue
 
     if(running != &idleProc) {
-        pcbSaved->segment = segment;
-        pcbSaved->stackPointer = sp;
-        pcbSaved->state = READY;
+        if(running->state == DEFUNCT) {
+            pcbToRun = removeFromReady();
+            pcbToRun->state = RUNNING;
+            running = pcbToRun;
 
-        addToReady(pcbSaved);
-        pcbToRun = removeFromReady();
-        pcbToRun->state = RUNNING;
-        running = pcbToRun;
+            returnFromTimer(running->segment, running->stackPointer);
+        } else {
+            pcbSaved->segment = segment;
+            pcbSaved->stackPointer = sp;
+            pcbSaved->state = READY;
 
-        returnFromTimer(running->segment, running->stackPointer);
+            addToReady(pcbSaved);
+            pcbToRun = removeFromReady();
+            pcbToRun->state = RUNNING;
+            running = pcbToRun;
+
+            returnFromTimer(running->segment, running->stackPointer);
+        }
     } else {
         pcbToRun = removeFromReady();
         if(pcbToRun != NULL) {
