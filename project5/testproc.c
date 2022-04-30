75f8810725d8ee86f5bb98d546916ea2be4c7fc8
iawong <ianwong1020@gmail.com>
2022-04-30 15:42:04 -0400

    added more tests for proc.c, setment fault for releasePCB test

--- project5/testproc.c
+++ project5/testproc.c
@@ -30,8 +30,88 @@ void testInit() {
 	assert(idleProc.stackPointer == 0x0000); 
 }
 
+void testGetFreeMemorySegment() {
+	int index, i;
+
+	initializeProcStructures();
+
+	index = getFreeMemorySegment();
+	assert(index == 0);
+	assert(memoryMap[index] == USED);
+
+	for(i = 0; i < 8; i++) {
+		memoryMap[i] = USED;
+	}
+	index = getFreeMemorySegment();
+	assert(index == -1);
+
+}
+
+void testReleaseMemorySegment() {
+	int index;
+
+	initializeProcStructures();
+
+	index = getFreeMemorySegment();
+	assert(memoryMap[index] == USED);
+	releaseMemorySegment(index);
+	assert(memoryMap[index] == FREE);
+}
+
+void testGetFreePCB() {
+	struct PCB *pcb;
+	int i;
+
+	initializeProcStructures();
+
+	pcb = getFreePCB();
+
+	assert(pcb->state == STARTING);
+	assert(pcbPool[0].state == STARTING);
+
+	for(i = 0; i < 8; i++) {
+		pcbPool[i].state = STARTING;
+	}
+	pcb = getFreePCB();
+	assert(pcb == NULL);
+}
+
+void testReleasePCB() { //segmentation fault
+	struct PCB *head, *middle, *tail;
+
+	head->next = middle;
+	middle->prev = head;
+	middle->next = tail;
+	tail->prev = middle;
+
+	middle->name[0] = 'a';
+	middle->state = STARTING;
+
+	releasePCB(middle);
+	assert(middle->name[0] == 0x00);
+	assert(middle->state == DEFUNCT);
+	assert(middle->next == NULL);
+	assert(middle->prev == NULL);
+}
+
+void testAddToReady() {
+
+}
+
+void testRemoveFromReady() {
+
+}
+
 int main() {
 	printf("Testing initializeProcStructures\n");
 	testInit();
+	printf("Testing getFreeMemorySegment\n");
+	testGetFreeMemorySegment();
+	printf("Testing releaseMemorySegment\n");
+	testReleaseMemorySegment();
+	printf("Testing getFreePCB\n");
+	testGetFreePCB();
+	printf("Testing releasePCB\n");
+	testReleasePCB();
 	printf("done\n");	
 }
