c88b504a0b4f9bbf237cf80ea503843d0bdd22a9
iawong <ianwong1020@gmail.com>
2022-05-03 00:03:34 -0400

    fixing files

--- project5/uprog1.c
+++
@@ -1,5 +0,0 @@
-main() {
-    enableInterrupts();
-    interrupt(0x21, 0x00, "WooHoo! I'm a user program!\r\n\0", 0, 0);
-    while(1);
-}
--- project5/uprog2.c
+++
@@ -1,6 +0,0 @@
-main() {
-    enableInterrupts();
-    interrupt(0x21, 0x00, "UPROG2\r\n\0", 0, 0);
-    interrupt(0x21, 0x05, 0, 0, 0);
-    while(1);
-}
